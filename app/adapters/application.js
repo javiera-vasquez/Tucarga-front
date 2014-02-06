48352051d95b50f8ecda85e1b4ccdb140f2654f2
Javier Enrique <s0wkete@gmail.com>
2014-02-05 23:35:26 -0300

    Refactoring
    Se deprecan vistas de prueba en ember kit se agregan las de tucarga. se
    rutean las imagenes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,1 +1,15 @@
+//App.Store = DS.Store.extend();
 export default DS.FixtureAdapter.extend();
+
+// Fixture
+//App.ApplicationAdapter = DS.FixtureAdapter;
+
+// Rest ember
+// App.ApplicationAdapter = DS.RESTAdapter.extend({
+//     host: 'http://localhost:8000/directory'
+// });
+
+// Rest Django
+// App.ApplicationAdapter = DS.DjangoRESTAdapter.extend({
+//    host: 'http://localhost:8000/directory'
+// });
--- app/router.js
+++ app/router.js
@@ -1,11 +1,36 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
-Router.map(function() {
-  this.route('component-test');
-  this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+// Router.map(function() {
+//   this.route('component-test');
+//   this.route('helper-test');
+//   // this.resource('posts', function() {
+//   //   this.route('new');
+//   // });
+// });
+
+Router.map(function () {
+    // Inicio
+    this.route('index', { path: '/' });
+    //Directorio transportistas
+    this.resource('directorio');
+    // Registro de usuario
+    this.resource('registro', function(){
+        this.route('gracias');
+    });
+    // Cotizar
+    this.resource('cotizar', function(){
+        this.route('exito');
+    });
+    // Como funciona
+    this.resource('como_funciona', function(){
+        this.resource('cargador');
+        this.resource('transportista');
+    });
+    // Nosotros
+    this.resource('nosotros', function(){
+        this.resource('contacto');
+        this.resource('ayuda');
+    });
 });
 
 export default Router;
